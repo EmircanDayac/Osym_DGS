f2d1ae6e0d4d10a1a4295b9546131fd22b51c59e
Emircan Dayac <emircandyc@outlook.com.tr>
2022-09-05 22:47:17 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+import discord
+
+context = ssl._create_unverified_context()
+res = urlopen("https://www.osym.gov.tr/TR,23771/2022.html",
+              context=context)
+soup = BeautifulSoup(res, "html.parser")
+data = soup.findAll("table", "table")
+
+token = "MTAxNjQwMDIzODk1ODg3ODcyMA.GCButd.sY65vv-RNUeTRpJH3YpW37nyUGUzMz3laXiBhU"
+client = discord.Client(intents=discord.Intents.default())
+
+
+@client.event
+async def on_ready():
+    print(f"Bot logged in as {client.user}")
+
+
+@client.event
+async def on_message(msg):
+    if msg.author != client.user:
+        if msg.content.lower().startswith("?yeter"):
+            for i in range(len(data)):
+                await msg.channel.send(f"yeterse yeter lan\n  {data[i].text.strip().replace('  ', '')}")
+
+
+client.run(token)
